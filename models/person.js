1d496c669c6d0827202ac4d570f9b463cc238faf
yichen0 <pp362886@gmail.com>
2017-04-28 09:16:39 +0800

    Add files via upload

---
+++ models/person.js
@@ -0,0 +1,14 @@
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://yclin:0729@ds159050.mlab.com:59050/u73');
+
+
+var Schema = mongoose.Schema;
+
+var personSchema = new Schema({
+  firstname : String,
+  adress : String,
+  suggestion : String
+ 
+});
+
+module.exports = mongoose.model('Person', personSchema);
